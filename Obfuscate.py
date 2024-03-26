be8828c38be1a0a1b75c8f540d4c9d3bab9bc7b0
Nguyen Minh Tien <minhtientiny@gmail.com>
2024-03-26 07:10:12 +0700

    Add files via upload

--- Obfuscate.py
+++ Obfuscate.py
@@ -1,1 +1,176 @@
-print("Hello World")
+import os
+import sys
+import zlib
+import time
+import base64
+import marshal
+import py_compile
+
+# Select raw_input() or input()
+if sys.version_info[0]==2:
+    _input = "raw_input('%s')"
+elif sys.version_info[0]==3:
+    _input = "input('%s')"
+else:
+    sys.exit("\n Phiên bản Python của bạn không được hỗ trợ!")
+
+# Encoding
+zlb = lambda in_ : zlib.compress(in_)
+b16 = lambda in_ : base64.b16encode(in_)
+b32 = lambda in_ : base64.b32encode(in_)
+b64 = lambda in_ : base64.b64encode(in_)
+mar = lambda in_ : marshal.dumps(compile(in_,'<x>','exec'))
+note = """#⠀⠀⠀⠀⠀⠀⠀⠀⣠⣴⣶⡋⠉⠙⠒⢤⡀⠀⠀⠀⠀⠀⢠⠖⠉⠉⠙⠢⡄⠀
+#⠀⠀⠀⠀⠀⠀⢀⣼⣟⡒⠒⠀⠀⠀⠀⠀⠙⣆⠀⠀⠀⢠⠃⠀⠀⠀⠀⠀⠹⡄
+#⠀⠀⠀⠀⠀⠀⣼⠷⠖⠀⠀⠀⠀⠀⠀⠀⠀⠘⡆⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⢷
+#⠀⠀⠀⠀⠀⠀⣷⡒⠀⠀⢐⣒⣒⡒⠀⣐⣒⣒⣧⠀⠀⡇⠀      ⠀ ⠀⢸
+#⠀⠀⠀⠀⠀⢰⣛⣟⣂⠀⠘⠤⠬⠃⠰⠑⠥⠊⣿⠀⢴⠃⠀⠀NTR    ⢸
+#⠀⠀⠀⠀⠀⢸⣿⡿⠤⠀⠀⠀⠀⠀⢀⡆⠀⠀⣿⠀⠀⡇⠀        ⣸
+#⠀⠀⠀⠀⠀⠈⠿⣯⡭⠀⠀⠀⠀⢀⣀⠀⠀⠀⡟⠀⠀⢸        ⢠⠏
+#⠀⠀⠀⠀⠀⠀⠀⠈⢯⡥⠄⠀⠀⠀⠀⠀⠀⡼⠁⠀⠀⠀⠳⢄⣀⣀⣀⡴⠃⠀
+#⠀⠀⠀⠀⠀⠀⠀⠀⠀⢱⡦⣄⣀⣀⣀⣠⠞⠁⠀⠀⠀⠀⠀⠀⠈⠉⠀⠀⠀⠀
+#⠀⠀⠀⠀⠀⠀⠀⢀⣤⣾⠛⠃⠀⠀⠀⢹⠳⡶⣤⡤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+#⠀⠀⠀⠀⣠⢴⣿⣿⣿⡟⡷⢄⣀⣀⣀⡼⠳⡹⣿⣷⠞⣳⠀⠀⠀⠀⠀⠀⠀⠀
+#⠀⠀⠀⢰⡯⠭⠹⡟⠿⠧⠷⣄⣀⣟⠛⣦⠔⠋⠛⠛⠋⠙⡆⠀⠀⠀⠀⠀⠀⠀
+#⠀⠀⢸⣿⠭⠉⠀⢠⣤⠀⠀⠀⠘⡷⣵⢻⠀⠀⠀⠀⣼⠀⣇⠀⠀⠀⠀⠀⠀⠀
+#⠀⠀⡇⣿⠍⠁⠀⢸⣗⠂⠀⠀⠀⣧⣿⣼⠀⠀⠀⠀⣯⠀⢸⠀⠀⠀⠀⠀⠀⠀
+#This script has been licensed using NguyenTienRosie
+#NguyenTienRosie v3.5, python whitelisting system by (Telegram) @mingnitetiny
+#https://rosieteam.net & https://nguyentienrosie.tokyo
+#Time: %s\n""" % time.ctime()                                   
+def banner(): # Program Banner
+    print("""\033[1;35m================================================================
+\033[1;31m███╗   ██╗████████╗██████╗ 
+\033[1;32m████╗  ██║╚══██╔══╝██╔══██╗
+\033[1;33m██╔██╗ ██║   ██║   ██████╔╝
+\033[1;34m██║╚██╗██║   ██║   ██╔══██╗
+\033[1;35m██║ ╚████║   ██║   ██║  ██║
+\033[1;36m╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝ Obfuscate Version: 3.5\n\033[1;35m================================================================\033[1;31m""")
+def menu(): # Program Menu
+    print("\x20\x5b\x30\x31\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x4d\x61\x72\x73\x68\x61\x6c\x0a\x20\x5b\x30\x32\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x5a\x6c\x69\x62\x0a\x20\x5b\x30\x33\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x42\x61\x73\x65\x31\x36\x0a\x20\x5b\x30\x34\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x42\x61\x73\x65\x33\x32\x0a\x20\x5b\x30\x35\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x42\x61\x73\x65\x36\x34\x0a\x20\x5b\x30\x36\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x5a\x6c\x69\x62\x2c\x42\x61\x73\x65\x31\x36\x0a\x20\x5b\x30\x37\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x5a\x6c\x69\x62\x2c\x42\x61\x73\x65\x33\x32\x0a\x20\x5b\x30\x38\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x5a\x6c\x69\x62\x2c\x42\x61\x73\x65\x36\x34\x0a\x20\x5b\x30\x39\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x4d\x61\x72\x73\x68\x61\x6c\x2c\x5a\x6c\x69\x62\x0a\x20\x5b\x31\x30\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x4d\x61\x72\x73\x68\x61\x6c\x2c\x42\x61\x73\x65\x31\x36\x0a\x20\x5b\x31\x31\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x4d\x61\x72\x73\x68\x61\x6c\x2c\x42\x61\x73\x65\x33\x32\x0a\x20\x5b\x31\x32\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x4d\x61\x72\x73\x68\x61\x6c\x2c\x42\x61\x73\x65\x36\x34\x0a\x20\x5b\x31\x33\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x4d\x61\x72\x73\x68\x61\x6c\x2c\x5a\x6c\x69\x62\x2c\x42\x31\x36\x0a\x20\x5b\x31\x34\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x4d\x61\x72\x73\x68\x61\x6c\x2c\x5a\x6c\x69\x62\x2c\x42\x33\x32\x0a\x20\x5b\x31\x35\x5d\x20\x45\x6e\x63\x6f\x64\x65\x20\x4d\x61\x72\x73\x68\x61\x6c\x2c\x5a\x6c\x69\x62\x2c\x42\x36\x34\x0a\x20\x5b\x31\x36\x5d\x20\x53\x69\x6d\x70\x6c\x65\x20\x45\x6e\x63\x6f\x64\x65\x0a\x20\x5b\x31\x37\x5d\x20\x45\x78\x69\x74\n")
+
+class FileSize: # Gets the File Size
+    def datas(self,z):
+        for x in ['Byte','KB','MB','GB']:
+            if z < 1024.0:
+                return "%3.1f %s" % (z,x)
+            z /= 1024.0
+    def __init__(self,path):
+        if os.path.isfile(path):
+            dts = os.stat(path).st_size
+            print("\033[1;40m [🌹] Encoded File Size: %s\n" % self.datas(dts))
+# FileSize('rec.py')
+
+# Encode Menu
+def Encode(option,data,output):
+    loop = int(eval(_input % "\033[1;40m [🌹] Encode Count: "))
+    if option == 1:
+        xx = "mar(data.encode('utf8'))[::-1]"
+        heading = "_ = lambda __ : __import__('marshal').loads(__[::-1]);"
+    elif option == 2:
+        xx = "zlb(data.encode('utf8'))[::-1]"
+        heading = "_ = lambda __ : __import__('zlib').decompress(__[::-1]);"
+    elif option == 3:
+        xx = "b16(data.encode('utf8'))[::-1]"
+        heading = "_ = lambda __ : __import__('base64').b16decode(__[::-1]);"
+    elif option == 4:
+        xx = "b32(data.encode('utf8'))[::-1]"
+        heading = "_ = lambda __ : __import__('base64').b32decode(__[::-1]);"
+    elif option == 5:
+        xx = "b64(data.encode('utf8'))[::-1]"
+        heading = "_ = lambda __ : __import__('base64').b64decode(__[::-1]);"
+    elif option == 6:
+        xx = "b16(zlb(data.encode('utf8')))[::-1]"
+        heading = "_ = lambda __ : __import__('zlib').decompress(__import__('base64').b16decode(__[::-1]));"
+    elif option == 7:
+        xx = "b32(zlb(data.encode('utf8')))[::-1]"
+        heading = "_ = lambda __ : __import__('zlib').decompress(__import__('base64').b32decode(__[::-1]));"
+    elif option == 8:
+        xx = "b64(zlb(data.encode('utf8')))[::-1]"
+        heading = "_ = lambda __ : __import__('zlib').decompress(__import__('base64').b64decode(__[::-1]));"
+    elif option == 9:
+        xx = "zlb(mar(data.encode('utf8')))[::-1]"
+        heading = "_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__[::-1]));"
+    elif option == 10:
+        xx = "b16(mar(data.encode('utf8')))[::-1]"
+        heading = "_ = lambda __ : __import__('marshal').loads(__import__('base64').b16decode(__[::-1]));"
+    elif option == 11:
+        xx = "b32(mar(data.encode('utf8')))[::-1]"
+        heading = "_ = lambda __ : __import__('marshal').loads(__import__('base64').b32decode(__[::-1]));"
+    elif option == 12:
+        xx = "b64(mar(data.encode('utf8')))[::-1]"
+        heading = "_ = lambda __ : __import__('marshal').loads(__import__('base64').b64decode(__[::-1]));"
+    elif option == 13:
+        xx = "b16(zlb(mar(data.encode('utf8'))))[::-1]"
+        heading = "_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b16decode(__[::-1])));"
+    elif option == 14:
+        xx = "b32(zlb(mar(data.encode('utf8'))))[::-1]"
+        heading = "_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b32decode(__[::-1])));"
+    elif option == 15:
+        xx = "b64(zlb(mar(data.encode('utf8'))))[::-1]"
+        heading = "_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));"
+    else:
+        sys.exit("\n Invalid Option!")
+    
+    for x in range(loop):
+        try:
+            data = "exec((_)(%s))" % repr(eval(xx))
+        except TypeError as s:
+            sys.exit(" TypeError : " + str(s))
+    with open(output, 'w') as f:
+        f.write(note + heading + data)
+        f.close()
+
+# Special Encode
+def SEncode(data,output):
+    for x in range(5):
+        method = repr(b64(zlb(mar(data.encode('utf8'))))[::-1])
+        data = "exec(__import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(%s[::-1]))))" % method
+    z = []
+    for i in data:
+        z.append(ord(i))
+    sata = "_ = %s\nexec(''.join(chr(__) for __ in _))" % z
+    with open(output, 'w') as f:
+        f.write(note+ "exec(str(chr(35)%s));" % '+chr(1)'*10000)
+        f.write(sata)
+        f.close()
+    py_compile.compile(output,output)
+
+# Main Menu
+def MainMenu():
+    try:
+        os.system('clear') # os.system('cls')
+        banner()
+        menu()
+        try:
+            option = int(eval(_input % "\033[1;40m [🌹] Select Mode: "))
+        except ValueError:
+            sys.exit("\n Select Invalid !")
+        
+        if option > 0 and option <= 17:
+            if option == 17:
+                sys.exit("\n\033[1;40m Thank You For Using.")
+            os.system('clear') # os.system('cls')
+            banner()
+        else:
+            sys.exit('\n\033[1;40m Select Invalid !')
+        try:
+            file = eval(_input % "\033[1;40m [🌹] File Name: ")
+            data = open(file).read()
+        except IOError:
+            sys.exit("\n\033[1;40m File Not Found!")
+        
+        output = file.lower().replace('.py', '') + '_enc.py'
+        if option == 16:
+            SEncode(data,output)
+        else:
+            Encode(option,data,output)
+        print("\n\033[1;40m [🌹] Encrypted Successfully %s" % file)
+        print("\033[1;40m [🌹] Saved In %s" % output)
+        FileSize(output)
+    except KeyboardInterrupt:
+        time.sleep(1)
+        sys.exit()
+
+if __name__ == "__main__":
+    MainMenu()
